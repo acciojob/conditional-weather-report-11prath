952388b4f04a1e4a7c647e1115d04b9b9f7e0ed6
PrathviShetty <108prathvi.shetty@gamil.com>
2026-03-10 16:06:35 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import WeatherDisplay from "./WeatherDisplay";
+
+function App() {
+  const [weather] = useState({
+    temperature: 25,
+    conditions: "Sunny"
+  }); 
+
+  return (
+    <div>
+      <WeatherDisplay weather={weather} />
+    </div>
+  );
+}
+
+export default App;
